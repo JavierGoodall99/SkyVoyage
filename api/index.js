f89c60e566e309c10bb6701e0f2404e037709830
JavierGoodall <javiergoodall@outlook.com>
2023-03-14 15:00:29 +0200

    Update

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const {errorHandling} = require('./middleware/errorHandling');
 
 // Add a CORS header to all responses
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080/')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "*")
   res.header("Access-Control-Allow-Headers", "*")
