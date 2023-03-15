e5a4b9f8217032a955e363419b1999b5b170cd4f
JavierGoodall <javiergoodall@outlook.com>
2023-03-15 16:50:23 +0200

    Update

--- vv/src/components/Admin.vue
+++ vv/src/components/Admin.vue
@@ -1,82 +1,104 @@
-<template lang="">
-    <div>
-        
+<template>
+  <div>
+    <h1>Programs</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Program Name</th>
+          <th>Location</th>
+          <th>Period</th>
+          <th>Program Description</th>
+          <th>Image URL</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="program" v-for="program in programs" :key="program.id">
+          <td>{{ program.ProgramName }}</td>
+          <td>{{ program.Location }}</td>
+          <td>{{ program.Period }}</td>
+          <td>{{ program.ProgramDescription }}</td>
+          <td>{{ program.imgURL }}</td>
+          <td>
+            <button @click="editProduct(program)">Edit</button>
+            <button @click="deleteProduct(program)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="showAddModal()">Add Program</button>
+    <div v-if="showModal">
+      <h2>{{ modalTitle }}</h2>
+      <form @submit.prevent="submitForm">
+        <label for="program-name">Program Name:</label>
+        <input id="program-name" v-model="form.ProgramName" required />
+        <label for="location">Location:</label>
+        <input id="location" v-model="form.Location" required />
+        <label for="period">Period:</label>
+        <input id="period" v-model="form.Period" required />
+        <label for="program-description">Program Description:</label>
+        <textarea id="program-description" v-model="form.ProgramDescription" required></textarea>
+        <label for="img-url">Image URL:</label>
+        <input id="img-url" v-model="form.imgURL" required />
+        <button type="submit">{{ modalAction }}</button>
+        <button type="button" @click="cancelForm">Cancel</button>
+      </form>
     </div>
+  </div>
 </template>
+
 <script>
 export default {
-    
-}
-</script>
-<style lang="">
-    
-</style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- import axios from 'axios'
-  
-  export default {
-    data () {
-      return {
-        FirstName: '',
-        LastName: '',
-        Email: '',
-        UserPassword: '',
-        Address: '',
-        PhoneNumber: '',
-        userRole: 1,
-        message: ''
+  computed: {
+    programs() {
+      return this.$store.state.programs;
+    }
+  },
+  data() {
+    return {
+      showModal: false,
+      modalTitle: "",
+      modalAction: "",
+      form: {
+        ProgramName: "",
+        Location: "",
+        Period: "",
+        ProgramDescription: "",
+        imgURL: "",
+      },
+    };
+  },
+  created() {
+    this.$store.dispatch("fetchPrograms");
+  },
+  methods: {
+    showAddModal() {
+      this.modalTitle = "Add Program";
+      this.modalAction = "Add";
+      this.showModal = true;
+    },
+    showEditModal(program) {
+      this.modalTitle = "Edit Program";
+      this.modalAction = "Update";
+      this.form = { ...program };
+      this.showModal = true;
+    },
+    submitForm() {
+      if (this.modalAction === "Add") {
+        this.$store.dispatch("createProgram", this.form);
+      } else if (this.modalAction === "Update") {
+        this.$store.dispatch("updateProgram", this.form);
+      }
+      this.showModal = false;
+    },
+    cancelForm() {
+      this.showModal = false;
+    },
+    deleteProduct(program) {
+      if (confirm("Are you sure you want to delete this program?")) {
+        this.$store.dispatch("deleteProgram", program);
       }
     },
-    methods: {
-      async register () {
-        const userData = {
-          FirstName: this.FirstName,
-          LastName: this.LastName,
-          Email: this.Email,
-          UserPassword: this.UserPassword,
-          Address: this.Address,
-          PhoneNumber: this.PhoneNumber,
-          userRole: this.userRole
-        }
-        try {
-          console.log('Registration process triggered');
-          const response = await axios.post('/register', userData)
-          console.log(response.data);
-          this.message = response.data;
-          this.$router.push('/programs');
-        } catch (error) {
-          console.log(error.message);
-        }
-      }
-    }
-  } -->
+  },
+};
+</script>
--- vv/src/store/index.js
+++ vv/src/store/index.js
@@ -5,7 +5,8 @@ const bStoreURL = 'https://volunteerventures.onrender.com/'
 export default createStore({
   state: {
     user: null,
-    message: null
+    message: null,
+    programs: []
   },
   mutations: {
     setUser (state, payload) {
@@ -13,8 +14,22 @@ export default createStore({
     },
     setMessage (state, payload) {
       state.message = payload
+    },
+    setPrograms (state, programs) {
+      state.programs = programs;
+    },
+    addProduct(state, payload) {
+      state.products.push(payload);
+    },
+    updateProduct(state, payload) {
+      const index = state.products.findIndex(product => product.id === payload.id);
+      state.products[index] = payload;
+    },
+    deleteProduct(state, payload) {
+      const index = state.products.findIndex(product => product.id === payload.id);
+      state.products.splice(index, 1);
     }
-  },
+},
   actions: {
     async register (context, payload) {
       console.log(payload);
@@ -37,14 +52,54 @@ export default createStore({
     } else {
       context.commit('setMessage', err)
     }
-  }
   },
+  async fetchPrograms(context) {
+      const res = await axios.get(`${bStoreURL}programs`);
+      const { results, err, msg } = await res.data;
+      if(results) {
+        context.commit('setPrograms', results)
+      }else {
+        context.commit('setMessage', err || msg)
+      }
+    },
+    async addProduct(context) {
+      const res = await axios.post(`${bStoreURL}program`);
+      const { results, err, msg } = await res.data;
+      if(results) {
+        context.commit('setPrograms', results)
+      }else {
+        context.commit('setMessage', err || msg)
+      }
+    },
+    async updateProduct(context) {
+      const res = await axios.put(`${bStoreURL}program/:id`);
+      const { results, err, msg } = await res.data;
+      if(results) {
+        context.commit('setPrograms', results)
+      }else {
+        context.commit('setMessage', err || msg)
+      }
+    },
+    async deleteProduct(context) {
+      const res = await axios.delete(`${bStoreURL}program/:id`);
+      const { results, err, msg } = await res.data;
+      if(results) {
+        context.commit('setPrograms', results)
+      }else {
+        context.commit('setMessage', err || msg)
+      }
+    }
+    },
 
   getters: {
     getUser: state => state.user,
-    getMessage: state => state.message
-  },
-})
+    getMessage: state => state.message,
+    getPrograms: state => state.programs,
+    getProductById: state => id => {
+      return state.products.find(product => product.id === id);
+    }
+  }
+  })
 
 
 // export default {
